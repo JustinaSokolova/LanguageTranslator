34b2fd4200e3895d44e677ce388a8ba1b4ff366b
Justina Sokolova <Justinasal@yandex.ru>
2022-11-02 01:07:09 +0300

    add: favicon

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import "./style/style.css";
+import favicon from "./assets/favicon.png";
 import Translate from "./modules/translator.module";
 
 const translatorModule = new Translate();
 translatorModule.renderTranslate();
+
+const head = document.querySelector("head");
+const link = document.createElement("link");
+link.rel = "icon";
+link.href = favicon;
+head.append(link);
